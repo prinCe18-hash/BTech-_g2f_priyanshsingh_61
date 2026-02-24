f248a70fe73622d6a4ad3dc96491917dd9d87fd5
prinCe18-hash <priyanshs283@gmail.com>
2026-02-24 11:45:18 +0530

    Add files via upload

---
+++ equality.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter a=");
+    scanf("%d",&a);
+     printf("Enter b=");
+    scanf("%d",&b);
+
+    if(a==b)
+    {
+        printf("\na and b are equal");
+    
+    }
+    else
+    {
+         printf("\na and b are not equal");
+    
+    }
+}
